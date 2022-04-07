00d1b0f532d2bb3035839956fed122c8f3e64cc2
loloxdev <loris_labarre@outlook.fr>
2022-04-07 11:17:11 +0200

    Mise en place du JS pour les boutons, vidéo sect

---
+++ script.js
@@ -0,0 +1,114 @@
+function toggle_text1(event) {
+    var span1 = document.getElementsByClassName("plongée");
+    for (let i=0; i<span1.length;i++){
+        span1[i].style.display ="none";
+
+    }   
+    var button = event.target.id.split('_')[1];
+    var span2 = document.getElementById("span_"+button);
+    span2.style.display = "inline";
+}
+
+
+
+
+
+
+
+
+
+/* 
+
+
+function toggle_text1() {
+    var span1 = document.getElementById("span_txt1");
+    var span2 = document.getElementById("span_txt2");
+    var span3 = document.getElementById("span_txt3");
+    var span4 = document.getElementById("span_txt4");
+    if(span1.style.display == "none") {
+      span1.style.display = "inline";
+    } else {
+      span1.style.display = "none";
+    }
+
+    if(span2.style.display == "inline" || span3.style.display == "inline" || span4.style.display == "inline") {
+        span2.style.display = "none"
+        span3.style.display = "none"
+        span4.style.display = "none";
+    } else {
+        span2.style.display = "none";
+        span3.style.display = "none"
+        span4.style.display = "none"
+    }
+  }
+
+  function toggle_text2() {
+    var span1 = document.getElementById("span_txt1");
+    var span2 = document.getElementById("span_txt2");
+    var span3 = document.getElementById("span_txt3");
+    var span4 = document.getElementById("span_txt4");
+    
+    if(span2.style.display == "none") {
+      span2.style.display = "inline";
+    } else {
+      span2.style.display = "none";
+    }
+
+    if(span1.style.display == "inline" || span3.style.display == "inline" || span4.style.display == "inline") {
+        span1.style.display = "none"
+        span3.style.display = "none"
+        span4.style.display = "none";
+    } else {
+        span1.style.display = "none";
+        span3.style.display = "none"
+        span4.style.display = "none"
+    }
+  }
+
+  function toggle_text3() {
+    var span1 = document.getElementById("span_txt1");
+    var span2 = document.getElementById("span_txt2");
+    var span3 = document.getElementById("span_txt3");
+    var span4 = document.getElementById("span_txt4");
+    
+    if(span3.style.display == "none") {
+      span3.style.display = "inline";
+    } else {
+      span3.style.display = "none";
+    }
+
+    if(span1.style.display == "inline" || span2.style.display == "inline" || span4.style.display == "inline") {
+        span1.style.display = "none"
+        span2.style.display = "none"
+        span4.style.display = "none";
+    } else {
+        span1.style.display = "none";
+        span2.style.display = "none"
+        span4.style.display = "none"
+    }
+  }
+
+  function toggle_text4() {
+    var span1 = document.getElementById("span_txt1");
+    var span2 = document.getElementById("span_txt2");
+    var span3 = document.getElementById("span_txt3");
+    var span4 = document.getElementById("span_txt4");
+    
+    if(span4.style.display == "none") {
+      span4.style.display = "inline";
+    } else {
+      span4.style.display = "none";
+    }
+
+    if(span1.style.display == "inline" || span2.style.display == "inline" || span3.style.display == "inline") {
+        span1.style.display = "none"
+        span2.style.display = "none"
+        span3.style.display = "none";
+    } else {
+        span1.style.display = "none";
+        span2.style.display = "none"
+        span3.style.display = "none"
+    }
+  }
+  
+  */
